58add9a8d78ed284a798eda25e537c11061a71c3
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-06-14 19:49:57 +0530

    navbar created

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -2,12 +2,17 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
-  return (
-    <div className='header'>
+    return (
+        <div className='header'>
+            <ul>
+                <li><a href="#home">Skills</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
 
-    
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default Header
