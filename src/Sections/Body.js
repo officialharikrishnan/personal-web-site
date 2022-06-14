e6958323f1349b60843ccee2fc3ea8f1bc89abce
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-06-14 20:42:56 +0530

    body image and name added, button added

--- src/Sections/Body.js
+++ src/Sections/Body.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Body.css'
+import { Container, Row, Col } from 'react-grid-system';
+import image from '../images/IMG_0011.JPG'
+
+import './Body.css'
 function Body() {
-  return (
-    <div>Body</div>
-  )
+    return (
+        <div>
+
+            <Container>
+                <Row>
+                    <Col sm={4}>
+                        <img src={image} alt="" />
+                    </Col>
+                    <Col sm={8}>
+                        <h1>Hai, <br /> I am Harikrishnan</h1>
+                        <button>Hire Me</button>
+                        <button>Download CV</button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default Body
