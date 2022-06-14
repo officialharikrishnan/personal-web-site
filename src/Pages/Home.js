3206bb8d56f3ea5fbb1047734de079e5f7bee9c4
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-06-14 19:37:02 +0530

    header,body,footer added, body background added

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Body from '../Sections/Body'
+import Footer from '../Sections/Footer'
+import Header from '../Sections/Header'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
