d0d698e98a39a82832264b33e28f32d9b4703a7a
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-06-15 17:13:08 +0530

    contact section design completed

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import { Container, Row, Col } from 'react-grid-system';
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <Container>
+        <form action="">
+        <Row>
+          <Col sm={6}>
+            <i className="fa fa-user fa-2x"></i>
+            <input type="text" placeholder="Enter Username" />
+          </Col>
+          <Col sm={6}>
+          <i className="fa fa-envelope fa-2x"></i>
+            <input type="text" placeholder="Enter Email" />
+          </Col>
+          <Col sm={12}>
+          <i className="fa fa-pencil-square fa-2x"></i>
+            <textarea type="text"  placeholder="..." />
+          </Col>
+        </Row>
+        <button>Sent</button>
+        </form>
+      </Container>
+    </div>
   )
 }
 
