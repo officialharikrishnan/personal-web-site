daa8d0ded4daf0fec05d937dfb2acfa65874777a
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-06-15 17:40:48 +0530

    social icons added

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Footer.css'
 import { Container, Row, Col } from 'react-grid-system';
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Footer() {
   return (
     <div>
       <Container>
+        <h1>Contact</h1>
         <form action="">
         <Row>
           <Col sm={6}>
@@ -17,11 +25,19 @@ function Footer() {
           </Col>
           <Col sm={12}>
           <i className="fa fa-pencil-square fa-2x"></i>
-            <textarea type="text"  placeholder="..." />
+            <textarea type="text"  placeholder="...🖊" />
           </Col>
         </Row>
         <button>Sent</button>
         </form>
+        
+        <section>
+        <FontAwesomeIcon className='icon' icon={faFacebook} size="3x" />
+        <FontAwesomeIcon className='icon' icon={faInstagram} size="3x" />
+        <FontAwesomeIcon className='icon' icon={faLinkedin} size="3x" />
+        <FontAwesomeIcon className='icon' icon={faGithub} size="3x" />
+
+        </section>
       </Container>
     </div>
   )
